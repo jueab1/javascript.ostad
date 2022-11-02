9c434044589967f1f25ceec49c561e9148e34a0f
jueab1 <hoque273@gmail.com>
2022-11-01 20:01:17 -0800

    javascript practiseing

---
+++ bangladesh.js
@@ -0,0 +1,28 @@
+
+// examle of import system
+
+// import {China} from "./china.js";
+// let obj = new China();
+// obj.Mobilephone();
+
+
+// example of function import
+
+// import {ledtv} from "./china.js";
+//
+// ledtv();
+
+
+// variable example type
+
+// import {aircular} from "./china.js"
+//
+// console.log("aircular");
+
+
+// default example
+
+import  Dhaka from "./china.js"
+
+let obj=new Dhaka()
+obj.milk();
---
+++ china.js
@@ -0,0 +1,39 @@
+
+// example of class export syestem
+
+// class  China {
+//
+//     Mobilephone(){
+//         alert("Mobilephone imported from china")
+//     }
+//
+// }
+//
+// export {China}
+
+// example of function export system
+
+// function ledtv(){
+//
+//     console.log("ledtv imported form china")
+// }
+// export {ledtv}
+
+
+// example of variable type
+
+// const aircular = "sharp,singer,panasonic";
+//
+// export {aircular};
+
+
+// default example
+
+class China {
+
+    milk(){
+        alert("milk imported from the China")
+    }
+
+}
+export default China;
